f16d0ede38ac47768cd09f5dd791264782a4adea
Nono Geulin <veryblueviolin@gmail.com>
2025-12-01 13:19:49 +0100

    Project shell ready

---
+++ Tests/Days.cs
@@ -0,0 +1,162 @@
+﻿using AoCTools.Loggers;
+using AoCTools.Workers;
+using NUnit.Framework;
+using System.IO;
+
+namespace AoC2025_Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class Days
+    {
+        private string GetDataPath(int day, string append = "")
+        {
+            return Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Days", $"day{day:00}{append}");
+        }
+
+        private void TestOneStar(IWorker worker, string dataPath, long expectedResult, SeverityLevel logLevel = SeverityLevel.Always)
+        {
+            var work = worker.WorkOneStar(dataPath, logLevel);
+            Assert.That(work, Is.EqualTo(expectedResult), $"One Star returned {work}, expected {expectedResult}.");
+        }
+
+        private void TestOneStar(IWorker worker, string dataPath, string expectedResult)
+        {
+            var work = worker.WorkOneStar_String(dataPath, SeverityLevel.Never);
+            Assert.That(work, Is.EqualTo(expectedResult), $"One Star returned {work}, expected {expectedResult}.");
+        }
+
+        private void TestTwoStars(IWorker worker, string dataPath, long expectedResult, SeverityLevel logLevel = SeverityLevel.Always)
+        {
+            var work = worker.WorkTwoStars(dataPath, logLevel);
+            Assert.That(work, Is.EqualTo(expectedResult), $"Two Stars returned {work}, expected {expectedResult}.");
+        }
+
+        //[Test]
+        //public void Day01()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day01.LocationsComparator(), GetDataPath(1), 2066446);
+        //    TestTwoStars(new AoC2024.Workers.Day01.LocationsComparator(), GetDataPath(1), 24931009);
+        //}
+
+        //[Test]
+        //public void Day02()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day02.RedNoseAnalysis(), GetDataPath(2), 383);
+        //    TestTwoStars(new AoC2024.Workers.Day02.RedNoseAnalysis(), GetDataPath(2), 436);
+        //}
+
+        //[Test]
+        //public void Day02b()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day02.RedNoseAnalysis(), GetDataPath(2, "b"), 524);
+        //    TestTwoStars(new AoC2024.Workers.Day02.RedNoseAnalysis(), GetDataPath(2, "b"), 569);
+        //}
+
+        //[Test]
+        //public void Day03()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day03.Multiplicator(), GetDataPath(3), 155955228);
+        //    TestTwoStars(new AoC2024.Workers.Day03.Multiplicator(), GetDataPath(3), 100189366);
+        //}
+
+        //[Test]
+        //public void Day04()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day04.WordSearcher(), GetDataPath(4), 2549);
+        //    TestTwoStars(new AoC2024.Workers.Day04.WordSearcher(), GetDataPath(4), 2003);
+        //}
+
+        //[Test]
+        //public void Day05()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day05.Printer(), GetDataPath(5), 5964);
+        //    TestTwoStars(new AoC2024.Workers.Day05.Printer(), GetDataPath(5), 4719);
+        //}
+
+        //[Test]
+        //public void Day06() // 17s
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day06.PastPlaceAnalizer(), GetDataPath(6), 4973);
+        //    TestTwoStars(new AoC2024.Workers.Day06.PastPlaceAnalizer(), GetDataPath(6), 1482);
+        //}
+
+        //[Test]
+        //public void Day07()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day07.RopeBridge(), GetDataPath(7), 10741443549536);
+        //    TestTwoStars(new AoC2024.Workers.Day07.RopeBridge(), GetDataPath(7), 500335179214836);
+        //}
+
+        //[Test]
+        //public void Day08()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day08.Antennas(), GetDataPath(8), 329);
+        //    TestTwoStars(new AoC2024.Workers.Day08.Antennas(), GetDataPath(8), 1190);
+        //}
+
+        //[Test]
+        //public void Day09()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day09.Defragmentation(), GetDataPath(9), 6353658451014);
+        //    TestTwoStars(new AoC2024.Workers.Day09.Defragmentation(), GetDataPath(9), 6382582136592);
+        //}
+
+        //[Test]
+        //public void Day10()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day10.TrailFinder(), GetDataPath(10), 611);
+        //    TestTwoStars(new AoC2024.Workers.Day10.TrailFinder(), GetDataPath(10), 1380);
+        //}
+
+        //[Test]
+        //public void Day11()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day11.QuanticStoneBlinker(), GetDataPath(11), 217443);
+        //    TestTwoStars(new AoC2024.Workers.Day11.QuanticStoneBlinker(), GetDataPath(11), 257246536026785);
+        //}
+
+        //[Test]
+        //public void Day12()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day12.GardenFencer(), GetDataPath(12), 1467094);
+        //    TestTwoStars(new AoC2024.Workers.Day12.GardenFencer(), GetDataPath(12), 881182);
+        //}
+
+        //[Test]
+        //public void Day13()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day13.ClawMachineHacker(), GetDataPath(13), 38714);
+        //    TestTwoStars(new AoC2024.Workers.Day13.ClawMachineHacker(), GetDataPath(13), 74015623345775);
+        //}
+
+        //[Test]
+        //public void Day14()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day14.ToiletRobots { BathroomSize = new Coordinates(103, 101) },
+        //                GetDataPath(14), 211773366);
+        //    TestTwoStars(new AoC2024.Workers.Day14.ToiletRobots { BathroomSize = new Coordinates(103, 101) },
+        //                GetDataPath(14), 7344, SeverityLevel.Low);
+        //}
+
+        //[Test]
+        //public void Day15()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day15.AnglerFishWarehouse(), GetDataPath(15), 1437174);
+        //    TestTwoStars(new AoC2024.Workers.Day15.AnglerFishWarehouse { DoubleSize = true }, GetDataPath(15), 1437468);
+        //}
+
+        //[Test]
+        //public void Day16()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day16.ReindeerRace(), GetDataPath(16), 106512); // 30s
+        //    TestTwoStars(new AoC2024.Workers.Day16.ReindeerRace(), GetDataPath(16), 563); // 29s
+        //}
+
+        //[Test]
+        //public void Day17()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetDataPath(17), "7,3,5,7,5,7,4,3,0");
+        //}
+    }
+}
---
+++ Tests/Samples.cs
@@ -0,0 +1,182 @@
+﻿using AoCTools.Loggers;
+using AoCTools.Workers;
+using NUnit.Framework;
+using System.IO;
+
+namespace AoC2025_Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class Samples
+    {
+        private string GetSamplePath(int day, string append = "")
+        {
+            return Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Samples", $"sample{day:00}{append}");
+        }
+
+        private void TestOneStar(IWorker worker, string dataPath, long expectedResult)
+        {
+            var work = worker.WorkOneStar(dataPath, SeverityLevel.Never);
+            Assert.That(work, Is.EqualTo(expectedResult), $"One Star returned {work}, expected {expectedResult}.");
+        }
+
+        private void TestOneStar(IWorker worker, string dataPath, string expectedResult)
+        {
+            var work = worker.WorkOneStar_String(dataPath, SeverityLevel.Never);
+            Assert.That(work, Is.EqualTo(expectedResult), $"One Star returned {work}, expected {expectedResult}.");
+        }
+
+        private void TestTwoStars(IWorker worker, string dataPath, long expectedResult)
+        {
+            var work = worker.WorkTwoStars(dataPath, SeverityLevel.Never);
+            Assert.That(work, Is.EqualTo(expectedResult), $"Two Stars returned {work}, expected {expectedResult}.");
+        }
+
+        //[Test]
+        //public void Sample01()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day01.LocationsComparator(), GetSamplePath(1), 11);
+        //    TestTwoStars(new AoC2024.Workers.Day01.LocationsComparator(), GetSamplePath(1), 31);
+        //}
+
+        //[Test]
+        //public void Sample02()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day02.RedNoseAnalysis(), GetSamplePath(2), 2);
+        //    TestTwoStars(new AoC2024.Workers.Day02.RedNoseAnalysis(), GetSamplePath(2), 9);
+        //}
+
+        //[Test]
+        //public void Sample03()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day03.Multiplicator(), GetSamplePath(3), 322);
+        //    TestTwoStars(new AoC2024.Workers.Day03.Multiplicator(), GetSamplePath(3), 209);
+        //}
+
+        //[Test]
+        //public void Sample04()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day04.WordSearcher(), GetSamplePath(4), 18);
+        //    TestTwoStars(new AoC2024.Workers.Day04.WordSearcher(), GetSamplePath(4), 9);
+        //}
+
+        //[Test]
+        //public void Sample05()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day05.Printer(), GetSamplePath(5), 143);
+        //    TestTwoStars(new AoC2024.Workers.Day05.Printer(), GetSamplePath(5), 123);
+        //}
+
+        //[Test]
+        //public void Sample06()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day06.PastPlaceAnalizer(), GetSamplePath(6), 41);
+        //    TestTwoStars(new AoC2024.Workers.Day06.PastPlaceAnalizer(), GetSamplePath(6), 6);
+        //}
+
+        //[Test]
+        //public void Sample07()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day07.RopeBridge(), GetSamplePath(7), 3749);
+        //    TestTwoStars(new AoC2024.Workers.Day07.RopeBridge(), GetSamplePath(7), 11387);
+        //}
+
+        //[Test]
+        //public void Sample08()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day08.Antennas(), GetSamplePath(8, "_a"), 2);
+        //    TestOneStar(new AoC2024.Workers.Day08.Antennas(), GetSamplePath(8), 14);
+
+        //    TestTwoStars(new AoC2024.Workers.Day08.Antennas(), GetSamplePath(8, "_b"), 9);
+        //    TestTwoStars(new AoC2024.Workers.Day08.Antennas(), GetSamplePath(8), 34);
+        //}
+
+        //[Test]
+        //public void Sample09()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day09.Defragmentation(), GetSamplePath(9), 1928);
+        //    TestOneStar(new AoC2024.Workers.Day09.Defragmentation(), GetSamplePath(9, "_b"), 11);
+        //    TestOneStar(new AoC2024.Workers.Day09.Defragmentation(), GetSamplePath(9, "_c"), 641243);
+        //    TestOneStar(new AoC2024.Workers.Day09.Defragmentation(), GetSamplePath(9, "_d"), 91);
+            
+        //    TestTwoStars(new AoC2024.Workers.Day09.Defragmentation(), GetSamplePath(9), 2858);
+        //}
+
+        //[Test]
+        //public void Sample10()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day10.TrailFinder(), GetSamplePath(10), 36);
+        //    TestTwoStars(new AoC2024.Workers.Day10.TrailFinder(), GetSamplePath(10), 81);
+        //}
+
+        //[Test]
+        //public void Sample11()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day11.QuanticStoneBlinker(), GetSamplePath(11), 55312);
+        //    TestTwoStars(new AoC2024.Workers.Day11.QuanticStoneBlinker(), GetSamplePath(11), 65601038650482);
+        //}
+
+        //[Test]
+        //public void Sample12()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12, "_a"), 140);
+        //    TestOneStar(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12, "_b"), 772);
+        //    TestOneStar(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12), 1930);
+            
+        //    TestTwoStars(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12, "_a"), 80);
+        //    TestTwoStars(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12, "_b"), 436);
+        //    TestTwoStars(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12, "_c"), 236);
+        //    TestTwoStars(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12, "_d"), 368);
+        //    TestTwoStars(new AoC2024.Workers.Day12.GardenFencer(), GetSamplePath(12), 1206);
+        //}
+
+        //[Test]
+        //public void Sample13()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day13.ClawMachineHacker(), GetSamplePath(13), 480);
+        //    TestOneStar(new AoC2024.Workers.Day13.ClawMachineHacker(), GetSamplePath(13, "_a"), 261);
+        //}
+
+        //[Test]
+        //public void Sample14()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day14.ToiletRobots { BathroomSize = new Coordinates(7, 11) },
+        //                GetSamplePath(14), 12);
+        //    TestOneStar(new AoC2024.Workers.Day14.ToiletRobots { BathroomSize = new Coordinates(7, 11) },
+        //                GetSamplePath(14, "_one"), 0);
+        //}
+
+        //[Test]
+        //public void Sample15()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day15.AnglerFishWarehouse(), GetSamplePath(15, "_a"), 2028);
+        //    TestOneStar(new AoC2024.Workers.Day15.AnglerFishWarehouse(), GetSamplePath(15), 10092);
+            
+        //    TestTwoStars(new AoC2024.Workers.Day15.AnglerFishWarehouse { DoubleSize = true },
+        //        GetSamplePath(15, "_b"), 618);
+        //    TestTwoStars(new AoC2024.Workers.Day15.AnglerFishWarehouse { DoubleSize = true },
+        //        GetSamplePath(15), 9021);
+        //}
+
+        //[Test]
+        //public void Sample16()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day16.ReindeerRace(), GetSamplePath(16), 7036);
+        //    TestOneStar(new AoC2024.Workers.Day16.ReindeerRace(), GetSamplePath(16, "_b"), 11048);
+            
+        //    TestTwoStars(new AoC2024.Workers.Day16.ReindeerRace(), GetSamplePath(16), 45);
+        //    TestTwoStars(new AoC2024.Workers.Day16.ReindeerRace(), GetSamplePath(16, "_b"), 64);
+        //}
+
+        //[Test]
+        //public void Sample17()
+        //{
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetSamplePath(17, "_a"), string.Empty); // B = 1
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetSamplePath(17, "_b"), "0,1,2");
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetSamplePath(17, "_c"), "4,2,5,6,7,7,7,7,3,1,0"); // A = 0
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetSamplePath(17, "_d"), string.Empty); // B = 26
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetSamplePath(17, "_e"), string.Empty); // B = 44354
+        //    TestOneStar(new AoC2024.Workers.Day17.Compiler(), GetSamplePath(17), "4,6,3,5,6,3,5,2,1,0");
+        //}
+    }
+}
